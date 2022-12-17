ba79f10df3a08001d3a0786d0baa6bf40ad859de
GersonBlood <gersonfernandesferreira4@gmail.com>
2022-12-17 17:40:00 -0300

    segundo principio violado corrigido

--- src/segundo/Segundo.java
+++ src/segundo/Segundo.java
@@ -1,12 +1,16 @@
 package segundo;
 
-import java.util.Date;
-
 public class Segundo {
 
 	public void imprimeDataContratacao(Funcionario func) {
 		Date data = func.getDataContratacao();
 		String msg = data.toString();
+		String msg = func.getDataContratacaoFormatada();
 		System.out.println(msg);
+	}
+
+	@Override
+	public String toString() {
+		return "Segundo []";
 	}
 }
