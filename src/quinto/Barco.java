cbe336c6de76733c4b8567cb294bacec28ca9a92
GersonBlood <gersonfernandesferreira4@gmail.com>
2022-12-18 22:09:34 -0300

    quinto pricipio modificado

--- src/quinto/Barco.java
+++ src/quinto/Barco.java
@@ -1,8 +1,33 @@
 package quinto;
 
 public class Barco extends Automovel {
+	public class Barco implements Transporte {
 
-	public Barco(String placa, String modelo, String tipoCombustivel, String chassi) {
-		super("", modelo, tipoCombustivel, "");
-	}
+		public Barco(String placa, String modelo, String tipoCombustivel, String chassi) {
+			super("", modelo, tipoCombustivel, "");
+
+		private String modelo;
+		private String tipoCombustivel;
+
+		public Barco(tring modelo, String tipoCombustivel) {
+			super();
+			this.modelo = modelo;
+			this.tipoCombustivel = tipoCombustivel;
+		}
+
+		public String getModelo() {
+			return modelo;
+		}
+
+		public void setModelo(String modelo) {
+			this.modelo = modelo;
+		}
+
+		public String getTipoCombustivel() {
+			return tipoCombustivel;
+		}
+
+		public void setTipoCombustivel(String tipoCombustivel) {
+			this.tipoCombustivel = tipoCombustivel;
+		}
 }
