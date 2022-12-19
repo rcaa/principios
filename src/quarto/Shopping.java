9fc39f2d4bf74f3deb0a892c505c90b53907c215
GersonBlood <gersonfernandesferreira4@gmail.com>
2022-12-18 22:05:10 -0300

    quarto principio modificado

--- src/quarto/Shopping.java
+++ src/quarto/Shopping.java
@@ -3,15 +3,11 @@ package quarto;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Shopping {
+public List<Loja> getLojas() {
+	return lojas;
+}
 
-	private List<Loja> lojas;
-
-	public Shopping() {
-		lojas = new ArrayList<>();
-	}
-
-	public List<Loja> getLojas() {
-		return lojas;
-	}
+public void addLoja(Loja loja) {
+	this.getLojas().add(loja);
+}
 }
